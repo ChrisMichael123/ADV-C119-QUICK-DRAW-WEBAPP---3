2955be26ba80bbb5aee89d0598c63053faf69b8d
ChrisMichael123 <87139501+ChrisMichael123@users.noreply.github.com>
2022-04-02 10:47:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload(){
+    classifier=ml5.imageClassifier("DoodleNet");
+    }
+    function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+    }
+    function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    }
+    function Clear(){
+        background("white");
+        
+    }
+    function classifyCanvas(){
+        classifier.classify(canvas, gotResult);
+    }
+    function gotResult(error, results){
+        if (error) {
+            console.log(error);
+        } else {
+           console.log(results);
+            document.getElementById("resultlab").innerHTML= results[0].label;
+            document.getElementById("conlab").innerHTML= results[0].confidence;
+            utterThis=new SpeechSynthesisUtterance(results[0].label);
+            synth.speak(utterThis);
+    }}
